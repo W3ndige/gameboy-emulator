be8343d3b510c51119acb20ab7c4b95f3a508c04
W3ndige <w3ndige@gmail.com>
2018-10-25 12:49:33 +0200

    Add cxxopts library for parsing command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "../libs/cxxopts.hpp"
 #include "gameboy/gameboy.hpp"
 
-int main(int argv, char **argc) {
+int main(int argc, char **argv) {
+
+    bool debugger = false;
+    try {
+        cxxopts::Options options("Gameboy Emulator", "Simple emulator for Gameboy.");
+        options.add_options()
+        ("d,debugger", "Run with debugger enabled.", cxxopts::value<bool>(debugger))
+        ("r,rom", "ROM file of an cartridge", cxxopts::value<std::string>());
+        auto result = options.parse(argc, argv);
+    }
+
+    catch (const cxxopts::OptionException &exception) {
+        printf("Error parsing arguments: %s\n", exception.what());
+    }
 
     Gameboy gameboy;
     bool end = false;
